6b8a604929bdce37336cc5ed55743c67d883a22b
rixyo <saleh35-3063@diu.edu.bd>
2023-05-17 16:45:26 +0600

    Initial commit from npx thirdweb create

---
+++ pages/_app.tsx
@@ -0,0 +1,33 @@
+import { ThirdwebProvider } from "@thirdweb-dev/react";
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import Header from "../components/Header";
+import ThirdwebGuideFooter from "../components/ThirdwebGuideFooter";
+import "../styles/globals.css";
+
+// This is the chain your dApp will work on.
+const activeChain = "goerli";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ThirdwebProvider activeChain={activeChain}>
+      <Head>
+        <title>thirdweb Marketplace with Next.JS</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="description"
+          content="Learn How To Use Thirdweb's Marketplace with Next.JS To List Your NFTs For Sale, Accept Bids, and Buy NFTs"
+        />
+        <meta
+          name="keywords"
+          content="Thirdweb, Marketplace, NFT Marketplace Tutorial, NFT Auction Tutorial, How To Make OpenSea"
+        />
+      </Head>
+      <Header />
+      <Component {...pageProps} />
+      <ThirdwebGuideFooter />
+    </ThirdwebProvider>
+  );
+}
+
+export default MyApp;
